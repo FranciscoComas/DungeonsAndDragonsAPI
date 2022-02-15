7a7ae310b8526ccffa302750f5c5743a1c280906
server <fcomasgomez@gmail.com>
2022-02-15 22:10:09 +0100

    Added connection settings and simple test.

--- DungeonsAndDragons/DataBaseConnect/DataBaseConnect.cs
+++ DungeonsAndDragons/DataBaseConnect/DataBaseConnect.cs
@@ -6,16 +6,38 @@ namespace DungeonsAndDragons.DataBaseConnect
     {
         public DataBaseConnect()
         {
-
+            databaseConnect();
         }
 
         public bool databaseConnect()
         {
             string connetionString = null;
             SqlConnection cnn;
-            connetionString = "Data Source=145.1.202.191;Initial Catalog=DatabaseName;User ID=UserName;Password=Password";
+            connetionString = "Data Source=garciablasco.tk;Initial Catalog=dnd;User ID=sa;Password=DnB2022";
             cnn = new SqlConnection(connetionString);
-            return false;
+            try
+            {
+                using (cnn)
+                {
+                    var query = "select 1";
+                    Console.WriteLine("Executing: {0}", query);
+
+                    var command = new SqlCommand(query, cnn);
+
+                    cnn.Open();
+                    Console.WriteLine("SQL Connection successful.");
+
+                    command.ExecuteScalar();
+                    Console.WriteLine("SQL Query execution successful.");
+                    return true;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Failure: {0}", ex.Message);
+                return false;
+            }
         }
+
     }
 }
