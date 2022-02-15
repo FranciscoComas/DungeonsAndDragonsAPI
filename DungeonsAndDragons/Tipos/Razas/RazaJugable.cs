84c52502d7800477b39a680505b151ce8e9fc803
fcomas <fcomasgomez@gmail.com>
2022-02-15 21:38:23 +0100

    conexion base de datos a medias y cambiados tipos

---
+++ DungeonsAndDragons/Tipos/Razas/RazaJugable.cs
@@ -0,0 +1,15 @@
+﻿using DungeonsAndDragons.Tipos.TiposComunes;
+
+namespace DungeonsAndDragons.Razas
+{
+    public class RazaJugable
+    {
+        public string raza { get; set; }
+        public string subraza { get; set; }
+        public string tamaño { get; set; }
+        public int velocidad { get; set; }
+        public string[] habilidades { get; set; }
+        public string[] idiomas { get; set; }
+        public Estadisticas estadisticas { get; set; }
+    }
+}
---
+++ DungeonsAndDragons/Tipos/TiposComunes/Clase.cs
@@ -0,0 +1,8 @@
+﻿namespace DungeonsAndDragons.Tipos.TiposComunes
+{
+    public class Clase
+    {
+        public string clase { get; set; }
+        public int nivel { get; set; }
+    }
+}
